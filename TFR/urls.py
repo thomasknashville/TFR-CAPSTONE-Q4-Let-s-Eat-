7f5b38257563b8b6bd5fb92ca9751b8eb293cc91
Karen Thomas <68983668+thomasknashville@users.noreply.github.com>
2021-07-08 10:02:40 -0500

    what is happenningnggg

--- TFR/urls.py
+++ TFR/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from users import views as user_views
+from restaurants import views as rest_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', user_views.index, name='homepage'),
+    path('', rest_views.index, name='homepage'),
     path('login/', user_views.login_view, name='login'),
-    path('logout/', user_views.logout_view),
-    path('signup/', user_views.signup_view),
+    path('logout/', user_views.logout_view, name='logout'),
+    path('signup/', user_views.signup_view, name='signup'),
 ]
