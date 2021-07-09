aecd302ab76bb3fb37d02a531c1a24d84eb5eab5
Karen Thomas <68983668+thomasknashville@users.noreply.github.com>
2021-07-09 12:01:03 -0500

    Revert "Merge branch 'lucas' into main"
    This reverts commit c76bc6eded343ba2e571b9406da36d68e98bf989, reversing
    changes made to 0626a51272bf5cb67f3b4cad8f79ec2ec173d191.

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 
-from users.models import TFRUser
 
-class AddUserProfile(forms.Form):
-    email = forms.EmailField(null=True, blank=True)
-    bio = forms.TextField(max_length=250)
+class SignupForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput())
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput())
+    password = forms.CharField(widget=forms.PasswordInput)
