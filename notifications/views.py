0b87f986ed895159b0cb4d83c9039d7419828bab
Karen Thomas <68983668+thomasknashville@users.noreply.github.com>
2021-07-19 21:47:54 -0500

    carousel of flippy cards is working on landing page also resolved bug w notification and some other styling issues

--- notifications/views.py
+++ notifications/views.py
@@ -9,14 +9,13 @@ from restaurants.models import Restaurant
 
 
 
-def note(request, user_id, rest_id):
-    rest = Restaurant.objects.filter(id=rest_id)
+def note(request, user_id):
     notes = Notification.objects.all()
     unread = Notification.objects.filter(read=False, recipient=request.user)
     for note in unread:
         note.read = True
         note.save()
-    return render(request, 'other_user.html', {'notes': notes, 'unread':unread, 'rest':rest})
+    return render(request, 'other_user.html', {'notes': notes, 'unread':unread})
 
 def alert(request):
     unread = Notification.objects.filter(read=False, recipient=request.user)
