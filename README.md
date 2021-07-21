813f379c88b00f3fedbc81706954a4e5fe398e30
Karen Thomas <68983668+thomasknashville@users.noreply.github.com>
2021-07-20 19:10:52 -0500

    resolved final styling issues on profile page and changed redirect to home after fav or unfav

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # TFR-CAPSTONE-Q4
+
+Django-Python App "Let's Eat!" This project was done by Team Sun:
+
+Bobby Ellena
+Josh Wilson
+Lucas Smith
+Karen Thomas
+
+as a assignment for our Q4, End-Of-Curriculum capstone.
+
+Assignment Problem Statement: Game-ify and resolve the time-honored tradition of arguing about where to eat
+
+Part 1: Create a web-app where a user can login. User can select restaurants they would be willing to eat from that day.
+User can view and edit his/her profile and restaurant selections.
+
+Part 2: User can view profile of other users. The app can compare the lists of selected restaurants for those two users.
+Users can see the randomly selected choice of destination restaurant based on their preferred eateries.
+
+Additional Features: In users profile user can set or update his/her profile picture and email whenever they like. Users
+can view details about the restaurants including popularity on the app, location, and the menu. 
+
+Technologies Used:
+Python 
+Django 
+Bootstrap 
+
+Additional Python Modules Required: 
+django-mptt = "^0.12.0" 
+Pillow = "^8.3.1" 
+python-env= "^1.0.0" 
+python-dotenv = "^0.18.0" 
+autopep8 = "^1.5.7" 
+django-rest-framework = "^0.1.0" 
+
+Note : The Secret_Key requiredfor the execution and debugging of project is removed from the project code and stored in an environment variable. 
+You will need the team's go-ahead to build on or debug the project
+
+Usage: 
+
+python django_web_app/manage.py makemigrations
+
+python django_web_app/manage.py migrate
+
+python django_web_app/manage.py runserver
+
+In your web browser enter the address : http://localhost:8000 or http://127.0.0.1:8000/
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,5 @@
 from restaurants.models import Restaurant
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponseRedirect
 from users.models import TFRUser
 from notifications.models import Notification
 from django.views.generic import View
@@ -34,24 +34,26 @@ class RestaurantDetailView(LoginRequiredMixin, View):
             for restaurant_fav in user.favorites.all():
                 if restaurant_fav == restaurant:    
                     fav_num += 1
-                # else error 500 YOU ALREADY FAVORITED THIS 
         context = {'restaurant': restaurant, 'fav_num': fav_num}
         return render(request, template_name, context)
 
 class AddToFavsView(LoginRequiredMixin, View):
     def get(self, request, restaurant_id):
         restaurant = Restaurant.objects.get(id=restaurant_id)
-        request.user.favorites.add(restaurant)
-        restaurant.num_favs += 1
-        request.user.save()
-        restaurant.save()
-        return redirect(reverse('rest_detail', args=(restaurant_id,)))
+        if restaurant not in request.user.favorites.all():
+            request.user.favorites.add(restaurant)
+            restaurant.num_favs += 1
+            request.user.save()
+            restaurant.save()
+        return HttpResponseRedirect(reverse('homepage'))    
+        
 
 class RemoveFromFavs(LoginRequiredMixin, View):
     def get(self, request, restaurant_id):
         restaurant = Restaurant.objects.get(id=restaurant_id)
-        request.user.favorites.remove(restaurant)
-        restaurant.num_favs -= 1
-        request.user.save()
-        restaurant.save()
-        return redirect(reverse('rest_detail', args=(restaurant_id,)))
+        if restaurant in request.user.favorites.all():
+            request.user.favorites.remove(restaurant)
+            restaurant.num_favs -= 1
+            request.user.save()
+            restaurant.save()
+        return HttpResponseRedirect(reverse('homepage'))
